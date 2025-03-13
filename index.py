68dfcf5fb99d96fbc384b985879b58a015c10ead
mokone <codewithmokone@gmail.com>
2025-03-13 07:28:24 +0200

    added comments

--- index.py
+++ index.py
@@ -1,5 +1,10 @@
+# Prompt the user to enter the first number
 firstNumber = int(input("Enter your first number? "))
+
+# Prompt the user to enter the second number
 secondNumber = int(input("Enter your second number? "))
+
+# prompt the user to choose an operator
 operator = input("Choose your operand, (e.g. +, -, /, *)? ")
 
 if operator == "+":
